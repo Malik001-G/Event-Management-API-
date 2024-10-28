c68fc3175e50f5d4fde3f6a8a075779bba1fdd43
Malik001-G <adisamalikng0@gmail.com>
2024-10-28 03:48:22 +0100

    Queuing the mail process

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -36,7 +37,12 @@ class SendEventReminders extends Command
 
         $this->info("Found {$eventCount} {$eventLabel} .");
 
-        $events->each(fn($event) => $event->attendees->each(fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")));
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
+        ));
 
         $this->info('Reminder notification sent successfully');
     }
